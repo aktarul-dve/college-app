35c9c97955518c303d8f77cb8d3a49001cdb1850
aktarul-dve <islammdaktarul345@gmail.com>
2025-05-12 12:09:43 +0600

    update code

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ try {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "https://college-app-one.vercel.app/",
+  origin: "https://college-app-one.vercel.app",
   credentials:true,
   methods:["GET","POST","PUT","DELETE"],
 }));
