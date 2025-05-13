98c49284cb0c6ea3aaf7cdc5f5ddee76ffa0884a
aktarul-dve <islammdaktarul345@gmail.com>
2025-05-13 20:41:38 +0600

    code update

--- frontend/src/Home/Carousel.jsx
+++ frontend/src/Home/Carousel.jsx
@@ -22,7 +22,7 @@ function Carousel() {
 
   return (
     <div className="w-full overflow-x-hidden">
-    <div className="max-w-5xl mx-auto mt-8 px-4">
+    <div className="max-w-5xl mx-auto mt-8 px-2">
       <ResponsiveCarousel
         showThumbs={false}
         autoPlay
--- frontend/src/Home/Notice.jsx
+++ frontend/src/Home/Notice.jsx
@@ -61,7 +61,7 @@ function Notice() {
 
                    <button
                     onClick={() => downloadImage(notice)}
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-2 rounded"
                      >
                       Download Now
                      </button>
