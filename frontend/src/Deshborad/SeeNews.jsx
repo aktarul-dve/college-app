151e9f8b94728b1d57474b0034187d8ec8aeec53
aktarul-dve <islamaktarul345@gmail.com>
2025-11-01 20:02:54 +0600

    mm

--- frontend/src/Deshborad/SeeNews.jsx
+++ frontend/src/Deshborad/SeeNews.jsx
@@ -91,9 +91,9 @@ function SeeNews() {
     <div className="px-4 py-6">
       <h1 className="text-3xl font-bold text-center mb-8">News & Events</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1  gap-6">
         {displayedNews.map((news) => (
-          <div key={news._id} className="bg-white shadow-lg rounded-lg overflow-hidden">
+          <div key={news._id} className="flex bg-white shadow-lg rounded-lg overflow-hidden">
             {/* Thumbnail */}
             <img
               src={news.photo?.url}
