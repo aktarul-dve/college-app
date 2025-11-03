13c896b6660ef31237ec9ba8d2dba7e6a0e81b8a
aktarul-dve <islamaktarul345@gmail.com>
2025-11-03 10:22:54 +0600

    ee

--- frontend/src/Deshborad/CreateTeachers.jsx
+++ frontend/src/Deshborad/CreateTeachers.jsx
@@ -27,7 +27,7 @@ function CreateTeachers() {
     formData.append("photo", photo);
 
     try {
-      const response = await fetch("https://college-app-3.onrender.com/teachers", {
+      const response = await fetch("https://college-app-3.onrender.com/creatTeacher", {
         method: "POST",
         body: formData,
       });
